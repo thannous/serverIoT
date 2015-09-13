022cbb2ac3e54e563f33e0e34f450349f2eb8776
Thanh Chau <thannous@gmail.com>
2015-09-13 02:58:33 +0200

    test 5

--- logger.js
+++ logger.js
@@ -12,8 +12,10 @@ module.exports = function (req, res, next) {
 
     res.on('finish', function () {
         myLed.write(0);
+        console.log("je commence")
         setTimeout(function(){
             myLed.write(1);
+            console.log("je suis")
         }, 2000);
 
         var duration = +new Date() - starTime;
